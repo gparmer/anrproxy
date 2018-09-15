5b0a6fa5bb5c55278f9bc8bd2456488ae06e21bb
Gabe Parmer <gparmer@gwu.edu>
2018-09-15 19:27:59 -0400

    hacked together

--- download.js
+++ download.js
@@ -6,131 +6,135 @@ var _cardListHtml     = '';
 
 // loadCards() load cardDB from local storage
 function loadCards() {
-  _cardDB = localStorage.getItem('cardDB');
-  _cardDB = JSON.parse(_cardDB);
+    _cardDB = localStorage.getItem('cardDB');
+    _cardDB = JSON.parse(_cardDB);
 
-  if (!_cardDB) {
-    fetchAllCards();
-  } else {
-    buildList();
-  }
+    if (!_cardDB) {
+	fetchAllCards();
+    } else {
+	buildList();
+    }
 }
 
 // saveCards() save cardDB in local storage
 function saveCards() {
-  localStorage.setItem('cardDB', JSON.stringify(_cardDB));
+    localStorage.setItem('cardDB', JSON.stringify(_cardDB));
 }
 
 // reset alias
 function reset() {
-  return fetchAllCards();
+    return fetchAllCards();
 }
 
 // fetchAllCards() get cards from api
 function fetchAllCards() {
-  localStorage.removeItem('cardDB');
-  _cardListElem.html('<span class="text-muted" data-loading>loading cards ...</span>');
+    localStorage.removeItem('cardDB');
+    _cardListElem.html('<span class="text-muted" data-loading>loading cards ...</span>');
 
-  $.getJSON( "https://netrunnerdb.com/api/2.0/public/cards", function(response) {
-    _cardDB = {};
+    $.getJSON( "https://netrunnerdb.com/api/2.0/public/cards", function(response) {
+	_cardDB = {};
 
-    $.each(response.data, function(key, item) {
-    	var image = 'https://netrunnerdb.com/card_image/' + item.code + '.png';
+	$.each(response.data, function(key, item) {
+    	    var image = 'https://netrunnerdb.com/card_image/' + item.code + '.png';
 
-		// console.log(item);
+	    // console.log(item);
 
-		if (typeof item.image_url != "undefined") {
-			image = item.image_url;
-		}
+	    if (typeof item.image_url != "undefined") {
+		console.log(item.image_url);
+		// cardgamedb https images are broken, so hack this until anrdb fixes the links
+		image = item.image_url.replace(/^https:/, "http:");
+		console.log(image);
+	    }
 
-        _cardDB[item.title.toLowerCase().replace(':', '').replace(' ' , '__')] = {
-          code: item.code,
-          image: image
-        }
+            _cardDB[item.code] = {
+		code: item.code,
+		title: item.title,
+		image: image
+            }
+	});
+
+	console.log('cards fetched from api');
+	saveCards();
+	buildList();
     });
-
-    console.log('cards fetched from api');
-    saveCards();
-    buildList();
-  });
 }
 
 // buildList() generate image list
 function buildList() {
-  var html = '';
-  alert(_userInputElem.val());
-  var input = _userInputElem.val().toLowerCase().split(/\n/);
-  var unfound = 0;
-
-  if (!_cardDB) {
-    return false;
-  }
-
-  for (var i=0; i<input.length; i++) {
-    var cardname = $.trim(input[i]).replace(':', '').replace(' ', '__');
-
-    if (cardname in _cardDB) {
-      var card = _cardDB[cardname];
-      var newCard = '';
-
-      newCard += '<a href="https://netrunnerdb.com/en/card/' + card.code + '" title="' + cardname + '" target="_blank">';
-      newCard += '  <img src="' + card.image + '" alt="' + card.code + '" />';
-      newCard += '</a>';
-
-      // console.log(JSON.stringify(card) + ' ' + cardname);
-
-      // append
-      html += newCard;
-
-      // prepend
-      // html = newCard + html;
-    } else {
-      unfound++;
+    var html = '';
+    var deckInput = _userInputElem.val().toLowerCase();
+    var unfound = 0;
+    var deckidregex = /\d\d\d\d\d/;
+    if (!_cardDB) {
+	return false;
     }
-  }
 
-  // if (unfound > 0) {
-  //   html += '<p class="no-print text-muted">' + unfound + ' not found<p>';
-  // }
+    match = deckidregex.exec(deckInput);
+    if (match == null) return;
 
-  if (_cardListHtml != html) {
-    // save current html
-    _cardListHtml = html;
-    // show images html
-    _cardListElem.html(_cardListHtml);
-  }
+    deckid = match[0];
+    $.getJSON("https://netrunnerdb.com/api/2.0/public/decklist/" + deckid, function (response) {
+	input = response.data[0].cards;
+	keys = Object.keys(input);
+
+	for (var i=0; i<keys.length; i++) {
+	    if (keys[i] in _cardDB) {
+		var card = _cardDB[keys[i]];
+
+		for (var j=0; j<input[keys[i]]; j++) {
+		    var newCard = '';
+	    	    newCard += '<a href="https://netrunnerdb.com/en/card/' + card.code + '" title="' + card.title + '" target="_blank">';
+	    	    newCard += '  <img src="' + card.image + '" alt="' + card.code + '" />';
+	    	    newCard += '</a>';
+
+	    	    // console.log(JSON.stringify(card) + ' ' + cardname);
+
+	    	    // append
+	    	    html += newCard;
+		}
+	    }
+	}
+
+	if (_cardListHtml != html) {
+	    // save current html
+	    _cardListHtml = html;
+	    // show images html
+	    _cardListElem.html(_cardListHtml);
+	}
+    });
+    _userInputElem.val(deckid);
+
+    var newloc = location.origin + location.pathname + "?id=" + deckid;
+    $('#DeckURL').html("Share link to this deck: <a href=\"" + newloc + "\">" + newloc + "</a>");
 }
 
 // assignEvents() rebuild image list when textarea changes
 function assignEvents() {
-  $(document).on('input propertychange', _userInputElem, function() {
-    buildList();
-  });
+    $(document).on('input propertychange', _userInputElem, function() {
+	buildList();
+    });
 }
 
 function findGetParameter(parameterName) {
     var result = null,
-        tmp = [];
+    tmp = [];
     location.search
         .substr(1)
         .split("&")
         .forEach(function (item) {
-          tmp = item.split("=");
-          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
         });
     return result;
 }
 
-function fillDeck() {
+function fillDeckId() {
     var deckId = findGetParameter("id");
-    if (deckId == null) {
-	deckId = "52609";
-    }
-    alert(deckId);
     _userInputElem.text(deckId);
-    alert(_userInputElem.val());
 }
 
-fillDeck();
-assignEvents();
-loadCards();
+$(document).ready(function() {
+    fillDeckId();
+    assignEvents();
+    loadCards();
+});
